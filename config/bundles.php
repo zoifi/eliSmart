453545a33413ed6b1b48a8219d7b417093d486a6
Martina Stoyanova <zoifi5313@gmail.com>
2021-02-15 17:19:36 +0200

    Default working copy

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['dev' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
